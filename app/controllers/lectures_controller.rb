b6f9c39cabb0b9cdb421935f7658c901f3a80388
ghadakhamis <ghada.khamis.m@gmail.com>
2018-05-07 15:20:26 +0200

    check if current user is the user of course

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -7,6 +7,23 @@ class LecturesController < InheritedResources::Base
 
   def show
     @lecture = Lecture.includes(:course).find(params[:id])
+  end
+
+  def create
+    @lecture = Lecture.includes(:course).new(lecture_params)
+    if current_user.id == @lecture.course.user_id 
+      respond_to do |format|
+        if @lecture.save
+          format.html {redirect_to lectures_path, notice: 'Lecture was successfully created.'}
+        else
+          format.html { render :new }
+        end 
+      end 
+    else
+      respond_to do |format|
+        format.html {redirect_to lectures_path, notice: 'This course didn\'t create by you .'}
+      end  
+    end  
   end  
   private
 
